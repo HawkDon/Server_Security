f8ee93f28189deeea5ff31ddcf67851123b297a8
Mathias <mbj3400@gmail.com>
2018-11-08 12:40:21 +0100

    java backend

---
+++ SecurityProject/src/main/java/com/mycompany/mavenproject2/UserResource.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2;
+
+import com.google.gson.Gson;
+import entities.User;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Mathias BJ
+ */
+@Path("user")
+public class UserResource {
+Gson gson = new Gson();
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of UserResource
+     */
+    public UserResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.mycompany.mavenproject2.UserResource
+     * @return an instance of java.lang.String
+     */
+    @Path("/test")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getUser() {
+        User user = new User("Stefan", 34, "Perkerdræber");
+        return gson.toJson(user);
+    }
+
+    /**
+     * PUT method for updating or creating an instance of UserResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    }
+}
---
+++ SecurityProject/src/main/java/entities/User.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+public class User {
+private String name;
+private int age;
+private String hobby;
+
+    public User(String name, int age, String hobby) {
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
+    
+}
