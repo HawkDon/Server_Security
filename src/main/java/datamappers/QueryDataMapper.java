e93dd2e0e2c8f7ff0d9d5674f130e07ed2461cad
Hawk <cph-ol31@cphbusiness.dk>
2018-11-11 18:37:19 +0100

    Attempt on backup and more queries

--- src/main/java/datamappers/QueryDataMapper.java
+++ src/main/java/datamappers/QueryDataMapper.java
@@ -1,9 +1,9 @@
 package datamappers;
 
-import entities.DB;
-import entities.ErrorMessage;
-import entities.User;
+import entities.*;
+import logic.Restore;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +18,10 @@ public class QueryDataMapper {
         try {
             con.setAutoCommit(false);
             Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
             switch (table) {
                 case "users":
-                    ResultSet rs = st.executeQuery(query);
-                    while (rs.next()){
+                    while (rs.next()) {
                         User user = new User();
                         user.setUserName(rs.getString("username"));
                         user.setPassword(rs.getString("password"));
@@ -32,13 +32,48 @@ public class QueryDataMapper {
                         al.add(user);
                     }
                     break;
-
+                case "animals":
+                    while(rs.next()) {
+                        Animal animal = new Animal();
+                        animal.setId(rs.getInt("id"));
+                        animal.setGender(rs.getString("gender"));
+                        animal.setLocation(rs.getString("location"));
+                        animal.setName(rs.getString("name"));
+                        animal.setType(rs.getString("type"));
+                        al.add(animal);
+                    }
+                    break;
+                case "cars":
+                    while(rs.next()) {
+                        Car car = new Car();
+                        car.setId(rs.getInt("id"));
+                        car.setMake(rs.getString("make"));
+                        car.setModel(rs.getString("model"));
+                        car.setYear(rs.getString("year"));
+                        al.add(car);
+                    }
+                case "plants":
+                    while(rs.next()) {
+                        Plant plant = new Plant();
+                        plant.setId(rs.getInt("id"));
+                        plant.setFamily(rs.getString("family"));
+                        plant.setLocation(rs.getString("location"));
+                        plant.setName(rs.getString("name"));
+                        al.add(plant);
+                    }
+                case "movies":
+                    while(rs.next()) {
+                        Movie movie = new Movie();
+                        movie.setId(rs.getInt("id"));
+                        movie.setGenre(rs.getString("genre"));
+                        movie.setPublished(rs.getString("published"));
+                        movie.setTitle(rs.getString("title"));
+                    }
                 default:
-                    con.setAutoCommit(false);
-                    st.executeQuery(query);
+                    System.out.println("Query went through");
                     break;
             }
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Something went wrong in the datamapper - Query");
             System.out.println(e.getMessage());
         }
